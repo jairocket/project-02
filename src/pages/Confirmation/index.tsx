e69a41377bd0e8a35756ab7272b77a9f1dff12d2
Jailson Anjos <jailsonanjos@hotmail.com>
2023-02-20 18:06:28 -0300

    feat(): confirmation-page structure

--- src/pages/Confirmation/index.tsx
+++ src/pages/Confirmation/index.tsx
@@ -1,7 +1,63 @@
+import { Clock, CurrencyDollar, MapPin } from 'phosphor-react'
+import confirmation from '../../assets/confirmation.svg'
+import {
+  AdressContainer,
+  BlockInfo,
+  ClockBackGround,
+  ConfirmationContainer,
+  CurrencyBackGround,
+  FirstContainer,
+  PinBackground,
+} from './styles'
+
 export function Confirmation() {
   return (
     <div>
-      <p>Confirmation</p>
+      <h1>Uhu! Pedido Confirmado</h1>
+      <p>Agora é só aguardar que logo o café chega até você</p>
+
+      <ConfirmationContainer>
+        <FirstContainer>
+          <AdressContainer>
+            <BlockInfo>
+              <PinBackground>
+                <MapPin color="white" />
+              </PinBackground>
+              <div>
+                <p>
+                  Entrega em <span>Rua João Daniel Martinelli, 102</span>
+                </p>
+                <p>Farrapos - Porto Alegre, RS</p>
+              </div>
+            </BlockInfo>
+            <BlockInfo>
+              <ClockBackGround>
+                <Clock color="white" />
+              </ClockBackGround>
+              <div>
+                <p>Previsão de entrega</p>
+                <span>20 min - 30 min</span>
+              </div>
+            </BlockInfo>
+            <BlockInfo>
+              <CurrencyBackGround>
+                <CurrencyDollar color="white" />
+              </CurrencyBackGround>
+              <div>
+                <p>Pagamento na entrega</p>
+                <span>Cartão de Crédito</span>
+              </div>
+            </BlockInfo>
+          </AdressContainer>
+        </FirstContainer>
+
+        <div>
+          <img
+            src={confirmation}
+            alt="cute drawing of a delivery guy on a purple bike"
+          />
+        </div>
+      </ConfirmationContainer>
     </div>
   )
 }
--- src/pages/Confirmation/styles.ts
+++ src/pages/Confirmation/styles.ts
@@ -1,2 +1,53 @@
 import styled from 'styled-components'
-export const Container = styled.div``
+export const AdressContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  padding: 2.5rem;
+  justify-content: center;
+  width: 32.875rem;
+  height: 16.75rem;
+  gap: 2rem;
+`
+export const ConfirmationContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 6.375rem;
+  justify-content: space-between;
+  padding: 3rem 0;
+`
+export const FirstContainer = styled.div`
+  border: 1px;
+  border-color: ${(props) => props.theme['purple-dark']};
+  box-sizing: border-box;
+  border-radius: 6px 36px;
+  border-style: solid;
+`
+export const BlockInfo = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 0.75rem;
+  color: ${(props) => props.theme['text']};
+
+  span {
+    font-weight: 700;
+  }
+`
+const IconBackground = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 2rem;
+  height: 2rem;
+  border-radius: 50%;
+`
+export const PinBackground = styled(IconBackground)`
+  background-color: ${(props) => props.theme['purple']};
+`
+export const ClockBackGround = styled(IconBackground)`
+  background-color: ${(props) => props.theme['yellow']};
+`
+
+export const CurrencyBackGround = styled(IconBackground)`
+  background-color: ${(props) => props.theme['yellow-dark']};
+`
